64beb3ba656cf96eaf4f39462e9f4a4b6d217221
Jonas Geiler <git@jonasgeiler.com>
2026-08-09 21:21:51 +0200

    fix: validate manifest URL and prevent mixed content errors

--- src/lib/manifest-url.ts
+++ src/lib/manifest-url.ts
@@ -1,12 +1,33 @@
 import { manifestUrlInput } from "./elements.ts";
+import { displayError } from "../utils/error.ts";
 
 const MANIFEST_URL_PARAM = "manifestUrl";
 const searchParams = new URLSearchParams(location.search);
 const searchParamsManifestUrl = searchParams.get(MANIFEST_URL_PARAM);
 
-export let manifestUrl = searchParamsManifestUrl ||
-	// Default to example from https://iiif.onb.ac.at/gui/manifest.html.
-	"https://iiif.onb.ac.at/presentation/ABO/+Z196807705/manifest/";
+// Default to example from https://iiif.onb.ac.at/gui/manifest.html.
+export let manifestUrl = "https://iiif.onb.ac.at/presentation/ABO/+Z196807705/manifest/";
+
+if (searchParamsManifestUrl) {
+	try {
+		// Parse as URL to validate.
+		const searchParamsManifestUrlObj = new URL(searchParamsManifestUrl);
+
+		// Prevent mixed content errors.
+		if (searchParamsManifestUrlObj.protocol !== location.protocol) {
+			searchParamsManifestUrlObj.protocol = location.protocol;
+		}
+
+		// Overwrite default.
+		manifestUrl = searchParamsManifestUrlObj.toString();
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			displayError(error);
+		} else {
+			throw error;
+		}
+	}
+}
 
 // Update manifest URL input field.
 manifestUrlInput.value = manifestUrl;
