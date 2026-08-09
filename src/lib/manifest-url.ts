fcd8b6a1d53ebc9c7b6e0c815000bd2c8ceaf2c3
Jonas Geiler <git@jonasgeiler.com>
2026-08-09 18:59:28 +0200

    chore: default to example manifest from https://iiif.onb.ac.at/gui/manifest.html

--- src/lib/manifest-url.ts
+++ src/lib/manifest-url.ts
@@ -5,8 +5,8 @@ const searchParams = new URLSearchParams(location.search);
 const searchParamsManifestUrl = searchParams.get(MANIFEST_URL_PARAM);
 
 export let manifestUrl = searchParamsManifestUrl ||
-	// TODO: Default to example from https://iiif.onb.ac.at/gui/manifest.html.
-	"/temp-static-manifest.json"; //"https://iiif.onb.ac.at/presentation/ABO/+Z196807705/manifest/";
+	// Default to example from https://iiif.onb.ac.at/gui/manifest.html.
+	"https://iiif.onb.ac.at/presentation/ABO/+Z196807705/manifest/";
 
 // Update manifest URL input field.
 manifestUrlInput.value = manifestUrl;
